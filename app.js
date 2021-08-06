ee9beebdec276305a5a1e90d5e478bad8e11fb71
Kyrylo <kirillomelcenko411@gmail.com>
2021-08-06 13:38:28 +0300

    update

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const list = document.querySelector('ul')
 const todoInfo = document.querySelector('.todoInfo')
 const underLines = document.querySelector('.underLines')
 const itemNumber = document.querySelector('.itemNumber')
+const inputArrow = document.querySelector('.arrow')
+const clearBtn = document.querySelector('.clearCompleatedBtn')
 
 let todoList = [{
     value: 'hello',
@@ -10,27 +12,23 @@ let todoList = [{
     done: false
 }]
 
-const delTodoItem = (todoId) => {
-    const newTodoList = todoList.filter(elem => elem.id !== +todoId)
-    setTodoList(newTodoList)
+const deleteTodoItem = (todoId) => {
+    setTodoList(todoList.filter(elem => elem.id !== +todoId))
 }
 
-const addTodoItem = (itemValue, newTodoList) =>{
-    newTodoList.push({
+const addTodoItem = (itemValue) =>{
+    setTodoList([...todoList, {
         value: itemValue,
         id: Date.now(),
         done: false
-    })
-    setTodoList(newTodoList)
+    }])
 }
 
 const changeItemStatus = (id, status) => {
-    todoList.map( item => {
-        if(id === item.id){
-            return item.done = status          
-        } 
-    })
-    setTodoList(todoList)
+    let newTodoList  
+
+    newTodoList = todoList.map( item => id === item.id ? {...item, done: status} : item)
+    setTodoList(newTodoList)
 }
 
 const itemRender = () => {
@@ -43,20 +41,28 @@ const itemRender = () => {
         const checkInput = document.createElement('input')
         const checkmark = document.createElement('span')
         const itemText = document.createElement('p')
+        const itemInput = document.createElement('input')
 
+        itemInput.classList.add('itemInput')
         itemText.classList.add('itemText')
         checkInput.setAttribute('type', 'checkBox')
         label.classList.add('container')
         checkmark.classList.add('checkmark')
         
         li.dataset.todoId = elem.id
+       
+    
+        const editItem = editItemClosure(itemInput)
+
+        li.addEventListener('click',() => {
+            editItem()
+        })
 
         checkInput.addEventListener('change',(e) => {
             const id = +e.target.parentElement.parentElement.dataset.todoId
             const status = e.target.checked
             changeItemStatus(id, status)
-        } 
-        )
+        })
         
         if(!elem.done){
             itemText.classList.add('active')
@@ -66,32 +72,110 @@ const itemRender = () => {
         }
 
         delBtn.classList.add('delBtn')
-        delBtn.addEventListener('click', ({target:{parentElement:{dataset:{todoId}}}}) => delTodoItem(todoId) )
+        delBtn.addEventListener('click', ({target:{parentElement:{dataset:{todoId}}}}) => deleteTodoItem(todoId) )
 
         itemText.textContent = elem.value
         label.append(checkInput, checkmark)
-        li.append(label,itemText,delBtn)
+        li.append(label,itemText,itemInput,delBtn)
         list.append(li) 
     })    
 
     todoInput.value = ''
 }
 
+const editItemClosure = (input) => {    // редактирование созданного item по клику.С этим пунктом я еще не закончил .Не знаю как правильно называть замыкание
+    let lastClick = 0
+    return () => {
+
+        input.addEventListener('click', (e)=>{
+            e.stopPropagation()
+        }) 
+
+        const d = Date.now();
+
+        if(d - lastClick < 400) {
+            input.classList.add('itemInputVisible')    
+        } else {
+            input.classList.remove('itemInputVisible')
+        }
+        lastClick = d;
+     } 
+}
+
+
 todoInput.addEventListener('keydown', (keyPressed) => {
     const itemValue = todoInput.value.trim()
-    const newTodoList = todoList.slice() // не уверен, нужно это или нет. Так я создаю независимый массив и не мутирую исходный.
-    
+      
     if(!itemValue){
         todoInput.value = ''
         return
     }
 
     if (keyPressed.key === 'Enter') {
-        addTodoItem(itemValue, newTodoList) 
+        addTodoItem(itemValue) 
     }
 })
 
-const showTodoInfo = () => {
+const clearCompleated = () => { // очистка всех выполненых задач 
+    const checkedItems = []
+    todoList.map(item=>{
+       if(item.done === false){
+            return checkedItems.push(item)
+        }
+    })
+
+    setTodoList(checkedItems)
+}
+
+const updateClearBtn = () => {
+    const someItemStatus = todoList.some(item=>{
+        return item.done === true
+    })
+
+    if(someItemStatus){
+        clearBtn.classList.add('clearCompleatedBtnVisible')
+    } else {
+        clearBtn.classList.remove('clearCompleatedBtnVisible')
+    }
+}
+
+clearBtn.addEventListener('click', clearCompleated)
+
+
+inputArrow.addEventListener('click', ()=>{  // изменение статуса всех items по клику. Нужно немного доделать
+    const allItemsStatus = todoList.every(item => {
+       return item.done === true
+    })
+    changeAllItemsStatus(allItemsStatus)
+    changeInputArrowColor(allItemsStatus)
+    
+})
+
+const changeAllItemsStatus = (allItemsStatus) => {
+    let newTodoList
+
+    if(allItemsStatus){        
+        newTodoList = todoList.map(item => {
+            return {...item, done: false}
+        })
+    } else {
+        newTodoList = todoList.map(item => {
+           return {...item, done: true}
+        })
+    }
+    setTodoList(newTodoList)
+}
+
+const changeInputArrowColor = allItemsStatus => {
+    
+    if(allItemsStatus){
+        inputArrow.classList.remove('arrowDark') 
+    } else {
+        inputArrow.classList.add('arrowDark')
+    }
+}
+
+const updateTodoInfo = () => {
     if(todoList.length === 1){
         itemNumber.textContent = `${todoList.length} item left`
     }else{
@@ -107,18 +191,22 @@ const showTodoInfo = () => {
     }
 }
 
+const updateInputArrow = () => {
+    if(todoList.length){
+        inputArrow.classList.add('arrowVisible')
+    } else{
+        inputArrow.classList.remove('arrowVisible') 
+    }
+}
+
 const setTodoList = newTodoList => {
     todoList = newTodoList
     itemRender()
-
-    showTodoInfo()
+    
+    updateClearBtn()
+    updateTodoInfo()
+    updateInputArrow()
 }
 
 setTodoList(todoList)
 
-
-// 1. setTodoList
-// 2. checkbox checked
-// 3. delete, add функции переделай так, чтобы в них передавалась информация, которая относится к их предназначению, т. е. для удаления это будет id, а для добавление label. Также убери логику, которая не относится к тому, для чего они предназначены, т. е. какие-то проверки и т. п.
-// Это нужно сделать, потому что каждая функция не должна ничего большего делать, кроме того, что ожидается
-// Проверки, вытаскивание айди из ивента ты можешь делать в самих обработчиках
